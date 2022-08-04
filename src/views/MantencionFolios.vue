e6a467ce8bfd490ae0d95f3d919354c91d17553b
German Salinas <german.salinas@pisquera.cl>
2022-08-04 10:16:20 -0400

    con fol

--- src/views/MantencionFolios.vue
+++ src/views/MantencionFolios.vue
@@ -1,41 +1,25 @@
 <template>
-    <div class="grid grid-rows-12 grid-cols-3 gap-0 bg-plata text-sky-900 h-120 w-full rounded">
-        <div ></div>
-
-
-    </div>
- 
-         <div class="bg-fuchsia-900 text-amber-50 row-start-1 row-end-5 col-start-1 col-end-3 text-center">Propietario</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-1 row-end-2 col-start-3 col-end-6">{{userStore.nombreEmpresaPropietaria}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-1 row-end-2 col-start-5 col-end-8">{{userStore.rutEmpresaPropietaria}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-1 row-end-2 col-start-7 col-end-10">{{userStore.girocomercialEmpresaPropietaria}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-2 row-end-3 col-start-3 col-end-6">{{userStore.direccionEmpresaPropietaria}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-2 row-end-3 col-start-5 col-end-8">{{userStore.comunaEmpresaPropietaria}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-2 row-end-3 col-start-7 col-end-10">{{userStore.ciudadEmpresaPropietaria}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-3 row-end-4 col-start-3 col-end-6">{{userStore.paisEmpresaPropietaria}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-3 row-end-4 col-start-5 col-end-8">{{userStore.identificadorUsuario}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-3 row-end-4 col-start-7 col-end-10">{{userStore.nombreUsuario}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-3 row-end-4 col-start-7 col-end-10">{{userStore.identificadorEmpresaPropietaria}}</div>
-
-    <div class="grid grid-rows-4 grid-cols-12 gap-0 bg-fuchsia-900 text-amber-50 h-32 w-full rounded">
-         <div class="bg-fuchsia-900 text-amber-50 row-start-1 row-end-5 col-start-1 col-end-3 text-center">Cliente</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-1 row-end-2 col-start-3 col-end-6">{{ClienteStore.identificadorEmpresaCliente}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-1 row-end-2 col-start-5 col-end-8">{{ClienteStore.nombreEmpresaCliente}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-1 row-end-2 col-start-7 col-end-10">{{ClienteStore.girocomercialEmpresaCliente}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-2 row-end-3 col-start-3 col-end-6">{{ClienteStore.direccionEmpresaCliente}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-2 row-end-3 col-start-5 col-end-8">{{ClienteStore.comunaEmpresaCliente}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-2 row-end-3 col-start-7 col-end-10">{{ClienteStore.ciudadEmpresaCliente}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-3 row-end-4 col-start-3 col-end-6">{{ClienteStore.paisEmpresaCliente}}</div>
-         <div class="bg-orange-500 text-sky-900 h-8 row-start-3 row-end-4 col-start-5 col-end-8">{{ClienteStore.contactoEmpresaCliente}}</div>
-    </div>
+    <div class="flex h-full justify-center items-center">
+        <div class="flex relative grid grid-rows-12 grid-cols-4 gap-0 bg-blue-100 text-sky-900 h-full justify-center items-center h-96 w-1/2 rounded border-4 border-sky-900">
+            <div class="flex absolute top-0 row-start-1 row-end-1 col-start-1 col-end-5 h-8 w-full bg-sky-900 text-plata text-justify text-center">MANTENIMIENTO DE FOLIOS</div>
+            <div class="flex row-start-2 row-end-2 col-start-1 col-end-3 h-8 w-full">ENTIDAD DE TRABAJO</div>
+            <div class="flex row-start-2 row-end-2 col-start-3 col-end-5 h-8 w-full">{{userStore.nombreEmpresaPropietaria}}</div>
+            <div class="flex row-start-3 row-end-3 col-start-1 col-end-3 h-8 w-full">EMPRESA CLIENTE</div>
+            <div class="flex row-start-3 row-end-3 col-start-3 col-end-5 h-8 w-full">{{ClienteStore.nombreEmpresaCliente}}</div>
+            <div class="flex row-start-4 row-end-4 col-start-1 col-end-3 h-8 w-full">TIPO FOLIO</div>
+            <div class="flex row-start-4 row-end-4 col-start-3 col-end-5 h-8 w-full">tipo de folio o codigo</div>
+            <div class="flex row-start-5 row-end-5 col-start-1 col-end-3 h-8 w-full">NUMERACION FOLIO</div>
+            <div class="flex row-start-5 row-end-5 col-start-3 col-end-5 h-8 w-full">numeracion de folio</div>
+            <div class="flex row-start-6 row-end-6 col-start-1 col-end-3 h-8 w-full">BOTONES</div>
+            <div class="flex row-start-6 row-end-6 col-start-3 col-end-5 h-8 w-full">BOTONES</div>
+        </div>
+    </div>    
 </template>
-
 <script setup>
 import { useUserStore } from '../stores/user';
 import { useClienteStore} from '../stores/clienteStore';
   
 const userStore = useUserStore()
 const ClienteStore = useClienteStore()
-
 
 </script>
